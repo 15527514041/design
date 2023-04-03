6a31283392d393f52aefa1db250a0653a50321df
15527514041 <56599220+15527514041@users.noreply.github.com>
2023-04-03 06:52:00 +0000

    Deploying to gh-pages from @ 15527514041/design@7f2cebadec49e28991072259e13e0be999153edd 🚀

---
+++ assets/index-4e7aa26e.js
@@ -0,0 +1,1 @@
+import{u as a,a as n,o as s,c as u,b as e,F as r}from"./index-1b2acafd.js";const c=e("div",null,"home page...",-1),i={__name:"index",setup(l){const o=a();n();const t=()=>{o.push({name:"about",query:{id:1,name:"from home page"}})};return(m,p)=>(s(),u(r,null,[c,e("div",null,[e("button",{onClick:t},"跳转页面到 About")])],64))}};export{i as default};
